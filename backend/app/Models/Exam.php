7bd599b49b8d6790a57306de889ac88b1949d83b
rizuuuu <mdzakirafifh96999@gmail.com>
2025-10-07 14:15:05 +0700

    Backend 20% done

--- backend/app/Models/Exam.php
+++ backend/app/Models/Exam.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Exam extends Model
 {
-    //
+    protected $fillable = [
+        'id',
+        'title',
+        'description',
+        'duration_minutes',
+        'start_at',
+        'end_at'
+    ];
 }
