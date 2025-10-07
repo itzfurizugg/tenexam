a21b0aefdce42efd84bdc3b8ad27d1c05bf3852e
rizuuuu <mdzakirafifh96999@gmail.com>
2025-10-07 12:53:08 +0700

    Backend 30% done

---
+++ backend/app/Models/Question.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $fillable = ['exam_id','prompt','options','answer','score'];
+
+
+    protected $casts = [
+        'options' => 'array',
+    ];
+
+
+    public function exam()
+    {
+        return $this->belongsTo(Exam::class);
+    }
+}
